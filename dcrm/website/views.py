acd7315d8e065a1643d87aec43bfcb3f6ac3f482
Husna Hussein <husnakilalo2@gmail.com>
2023-05-22 20:05:59 +0300

    first commit

---
+++ dcrm/website/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
+
+# Create your views here.
+def home(request):
+    records = Record.objects.all()
+
+    # Check to see if logged in
+    if request.method == 'POST':
+        username =request.POST['username']
+        password =request.POST['password']
+        # Authenticate
+        user =authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Successfully Logged In")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was a Error Logging In, Please Try Again!")
+            return redirect('home')
+
+    else:
+        return render(request, 'home.html', {'records': records})
+         
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out!")
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username =form.cleaned_data['username']
+            password =form.cleaned_data['password1']
+            user =authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Been Successfully Registered")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You Must Be Logged In To VIew That Page")
+        return redirect('home')
