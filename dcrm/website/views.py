942ae192a829dd00bc5a4bba8732c58fb04d2a69
Husna Hussein <husnakilalo2@gmail.com>
2023-05-22 21:21:28 +0300

    updates

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 # Create your views here.
@@ -58,3 +58,30 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You Must Be Logged In To VIew That Page")
         return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Deleted Successfully")
+        return redirect('home')
+    else:
+        messages.success(request, "You Must Be Logged In To VIew That Page")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+         if request.method == 'POST':
+             if form.is_valid():
+                 add_record =form.save()
+                 messages.success(request, "Record Added Successfully")
+                 return redirect('home')
+                
+         return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You Must Be Logged In")
+        return redirect('home')
+
+
+
