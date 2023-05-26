b8e04f012d926563b41240b69e62471f5804f594
Husna Hussein <husnakilalo2@gmail.com>
2023-05-26 21:21:19 +0300

    Made changes to the project

--- website/views.py
+++ website/views.py
@@ -74,14 +74,27 @@ def add_record(request):
     if request.user.is_authenticated:
          if request.method == 'POST':
              if form.is_valid():
-                 add_record =form.save()
+                 add_record = form.save()
                  messages.success(request, "Record Added Successfully")
-                 return redirect('home')
-                
+                 return redirect('home')   
          return render(request, 'add_record.html', {'form':form})
     else:
         messages.success(request, "You Must Be Logged In")
         return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+         current_record = Record.objects.get(id=pk)
+         form = AddRecordForm(request.POST or None, instance = current_record)
+         if form.is_valid():
+             form.save()
+             messages.success(request, "Record Has Been Updated")
+             return redirect('home')
+         return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success(request, "You Must Be Logged In")
+        return redirect('home')
+
 
 
 
